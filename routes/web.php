814451e320cd1e6f77c0ed0366506271d5ba17fe
Gabriel Santos <gabi_drew@live.com>
2026-01-12 04:10:39 -0300

    Add Discord verification route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,9 @@ Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.upda
 Route::get('/archives', [TaskController::class, 'trash'])->name('tasks.trash');
 Route::put('/archives/{task}', [TaskController::class, 'restore'])->name('tasks.restore')->withTrashed();
 Route::delete('/archives/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy')->withTrashed();
+
+Route::get('/.well-known/discord', function () {
+    return Response::make('dh=ff3a218acc8af838cb6c85fba1904de6505d43d5', 200, [
+        'Content-Type' => 'text/plain'
+    ]);
+});
